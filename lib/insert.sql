cb81a54e49d8ab992850b8ce66fa4d5916780e45
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-03 10:58:03 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,42 @@
+INSERT INTO series ('title','author_id','subgenre_id') VALUES ('GOT','GRMARTIN','fantasy');
+INSERT INTO series ('title','author_id','subgenre_id') VALUES ('TVD', 'Unknown', 'Vampire');
+
+
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Game of Thones', '2020', '1');
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Storm of Swords',  '2022', '2');
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Clash of Kings', '2023', '3');
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Feast for Crows', '2024', '4');
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Dance with Dragons', '2025', '5');
+INSERT INTO books ('title', 'year', 'series_id') VALUES ('A Dance with Me', '2015', '6');
+
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+INSERT INTO characters ('name', 'species', 'motto', 'author_id') VALUES ('Edward', 'human', 'shrimp', '1');
+
+INSERT INTO subgenres ('name') VALUES ('meow');
+INSERT INTO subgenres ('name') VALUES ('meow');
+
+INSERT INTO authors ('title', 'name') VALUES ('meow', 'hello');
+INSERT INTO authors ('title', 'name') VALUES ('meow', 'hello');
+
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
+INSERT INTO character_books ('book_id', 'character_id') VALUES ('1', '2');
